d7bc4aed384aa7c400bbc10450129f1fb11ff1ab
Mark Needham <m.h.needham@gmail.com>
2013-01-21 23:50:55 +0000

    mark: sorted out the 3rd question

--- complete.R
+++ complete.R
@@ -7,3 +7,18 @@ complete <- function(directory, id = 1:332) {
   data.frame(id=id, nobs=nobs)
 }
 
+complete <- function(directory, id = 1:332) {  
+  readFile <- function(id) {
+    read.csv(paste(directory, "/", sprintf("%03d", as.numeric(id)), ".csv", sep=""))  
+  }
+  
+  totallyNA <- function(id) {
+    contents <- readFile(id)
+    contents[!is.na(contents$sulfate) & !is.na(contents$nitrate),]
+  }
+  
+  nobs <- apply(as.array(id),1, function(x) nrow(totallyNA(x)))
+  data.frame(id=id, nobs=nobs)
+}
+
+# blah[!is.na(blah$sulfate) & !is.na(blah$nitrate),]
